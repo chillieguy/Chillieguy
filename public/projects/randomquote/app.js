993f74f97badaa173c842206487420b1df87d628
Chuck Underwood <chillieguy@live.com>
2017-02-08 19:53:52 -0800

    Add projects/randomquote

---
+++ public/projects/randomquote/app.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', getQuote, false);
+
+var bgColors = ["SkyBlue", "PowderBlue", "Pink", "PaleGreen", "PaleTurquoise", "LightPink", "LightCyan", "LightBlue", "Cyan", "Aquamarine", "Aqua"];
+
+$('.get-quote').click(getQuote);
+
+$('.tweet').click(function() {
+  var twtText = $('.quote').text() + " " + $('.quote-author').text();
+  var twtLink = 'http://twitter.com/home?status=' + twtText;
+  
+ window.open(twtLink,'_blank');
+});
+
+function getQuote() {
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+    .done(update)
+    .fail(handleErr);  
+}
+
+function update(response) {
+  $('.quote').html("");
+  $('.quote').html(response.quoteText);
+  $('.quote-author').html("~ " + response.quoteAuthor);
+  changeBGColor();
+}
+
+function handleErr(jqxhr, textStatus, err) {
+  console.log("Request Failed: " + textStatus + ", " + err);
+}
+
+function changeBGColor() {
+  var color = bgColors[Math.floor(Math.random() * bgColors.length)];
+  $('body').css('background-color', color);
+}
+
